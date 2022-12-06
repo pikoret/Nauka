d12d138fd4a29f1927d9c72e50d6d88d2e8d8b70
Piotr <piotrchuchla1@gmail.com>
2022-12-06 19:55:19 +0100

    Initial commit

---
+++ Source.cpp
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <cstring>
+#include <cstdio>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <conio.h>
+#include <algorithm>
+#include <cctype>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <windows.h>
+#include <memory>
+
+
+
+std::unique_ptr<int> ptr = std::unique_ptr<int>(new int());
+
+
+using namespace std;
+
+
+
+int liczenieLinijekTekstuWPlikuTekstowym ( string nazwaPliku)
+{
+	int licznik=0;
+	string linijka;
+	ifstream plik ;
+	plik.open(nazwaPliku);
+	if(!plik.good())
+		return false;
+	while(!plik.eof())
+	{
+		getline(plik,linijka);
+		licznik++;
+	}
+	return licznik;
+}
+
+string WczytajNazwePliku()
+{
+	string nazwa;
+	nazwa="abc";
+	return nazwa;
+
+}
+
+int main()
+{
+	string dupa;
+	char tab[4];
+	dupa="dupa";
+	dupa = WczytajNazwePliku();
+	
+
+
+
+
+	int a;
+	cin>>a;
+	return 0;
+
+
+}
