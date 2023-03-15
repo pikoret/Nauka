db8d4650e373e77069e8eb211d48607eb8a7577a
root <piotrchuchla1@gmail.com>
2023-03-15 20:58:56 +0000

    15.03.2023

--- Program2/main.cpp
+++ Program2/main.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
 
+using namespace std;
+
+class Otaczjaca
+{
+public:
+string NapisPublic = "Public";
+
+private:
+string NapisPrivate = "Pivate";
+
+protected:
+string NapisProtected = "Protected";
+
+class Wewnetrzna
+{
+
+};
+
+
+
+};
+
+class Dziedziczaca:public Otaczjaca
+{
+    public:
+    void wyswietl()
+    {
+        cout<<NapisPublic;
+    }
+
+};
+
 int main()
 {
-    std::cout<<"hello\n";
-    return 0;
+  //  Dziedziczaca obiekt;
+  //  obiekt.wyswietl();
+
+    cout<<"Foo";
+  
+ return 0;
 }
