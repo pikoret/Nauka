53e24932b86d65d3402713ff7bf49f221fc7d965
root <piotrchuchla1@gmail.com>
2023-03-16 14:46:12 +0000

    16.03.2023

--- Program1/main.cpp
+++ Program1/main.cpp
@@ -2,6 +2,60 @@
 
 using namespace std;
 
+//Diamond problem
+class Person
+{
+  public:
+  Person()
+{
+  cout<<"Person"<<endl;
+
+}
+  ~Person()
+{
+  cout<<"~Person"<<endl;
+}
+};
+
+class Father: virtual public Person
+{
+  public:
+  Father()
+  {
+    cout<<"Father"<<endl;
+  }
+  ~Father()
+  {
+    cout<<"~Father"<<endl;
+  }
+
+};
+class Mother: virtual public Person
+{
+  public:
+  Mother()
+  {
+    cout<<"Mother"<<endl;
+  }
+  ~Mother()
+  {
+    cout<<"~Mother"<<endl;
+
+  }
+};
+class Child:public Mother, public Father
+{
+  public:
+  Child()
+  {
+    cout<<"Child"<<endl;
+  }
+  ~Child()
+  {
+    cout<<"~Child"<<endl;
+  }
+};
+
 class Otaczjaca
 {
 public:
@@ -48,10 +102,11 @@ class Dupa
 int main()
 {
     int a;
-    Dziedziczaca obiekt;
-    Otaczjaca obiektO;
-    obiekt.wyswietl();
+   // Dziedziczaca obiekt;
+   // Otaczjaca obiektO;
+   // obiekt.wyswietl();
    // cout<<obiekt.prot<<endl;
+   Child obiektCh;
     cout << "Foo";
   cin>>a;
  return 0;
